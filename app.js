c7443cd1077b46647d9c08c6818368c440d4b829
trung040971 <nmtrung@gmail.com>
2022-10-02 16:03:00 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((error, req, res, next) => {
+	return res.status(error.statusCode || 500).json({
+		message: error.message || "Internal Server Error",
+	});
+});
+
+module.exports = app;
